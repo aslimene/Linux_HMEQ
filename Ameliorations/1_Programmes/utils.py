84dd5fce447235f563dc86eb250f11832b937be7
aadmberrada <abdoulazizberrada@gmail.com>
2022-02-05 11:45:45 +0100

    Readme

--- Ameliorations/1_Programmes/utils.py
+++ Ameliorations/1_Programmes/utils.py
@@ -47,7 +47,7 @@ def prct_nan(x):
     --------
 
     Cette fonction permet de déterminer le pourcentage de valeurs manquantes des variables d'un DataFrame.
-    Elle calcule séparément les variables qaulitatives et qauntitatives.
+    Elle calcule séparément les variables qualitatives et quantitatives.
     
     Example
     --------
@@ -268,7 +268,51 @@ def selection_np(selector, estimateur, X_train, y_train, X_test, Forward = False
 def selection_list(selectors, algos, X_train, y_train, X_test, Forward = False, metric = "accuracy", 
                   smote = False, cv = 5):
     """
-    docstring à faire
+    Cette fonction affiche pour une liste de selectors et d'estimateurs, le nombre et les variables selectionnées de même que la métrique correspondant à chaque combinaison selector-estimator
+    Si le selector est la RFE ou ExhaustiveFeatureSelector, la fonction affiche le score obtenu après la sélection.
+    
+    Paramètres :
+    ------
+    selectors : list of selectors
+            Liste contenant des méthodes de sélection de variables,
+                
+    algos : list of estimators
+            Liste contenant Tout algorithme de classification type sklearn
+
+    X_train, y_train : DataFrame
+            Training data
+
+    X_test : DataFrame
+            Variables explicatives contenues dans le test set
+
+    Forward : bool (default: True)
+            Forward selection si True,
+    
+    print_estimateur : bool (default: False)
+            Affiche le nom de l'estimateur utilisé
+    
+    metric : str
+            Métrique à optimiser
+
+    smote : bool (default: False)
+            Oversampling si True
+
+    cv : int
+            Nombre de KFold
+    
+    Returns
+    ------
+    recap : DataFrame
+            Récapitulatifs des résultats 
+
+    Examples
+    ------
+    >>>from sklearn.linear_model import LogisticRegression, LinearRegression
+    >>>from sklearn.feature_selection import RFE, ExhaustiveFeatureSelector
+    >>>selectors = [RFE, ExhaustiveFeatureSelector]
+    >>>algos = [LogisticRegression, LinearRegression]
+    selection_list(selectors, algos, print_estimateur = True)
+
     """
     dictionnaire = {"Selector" : [], "Estimateur" : [], "Score" : [], "N_Variables": [], "Duree(s)" : [], "Variables" : []}
 
@@ -288,7 +332,17 @@ def selection_list(selectors, algos, X_train, y_train, X_test, Forward = False, 
     return recap
 
 def val(X, y, smote = False):
+    """
+    Parametres
+    ------
+
+    Returns
+    ------
+
+    Example
+    ------
     
+    """
     if smote == True : 
         title = 'Logit Regression with smote results'
         from imblearn.over_sampling import SMOTE
@@ -555,108 +609,3 @@ def grid_search(model, X_train, y_train):
     print("Le meilleur score AUC est: %.2f" %gs.best_score_)
     
     return 
-
-"""
-def model_comp(model, X_train, y_train, X_test, y_test, 
-          smote = True, show_roc = True, show_conf_matrix = False, cut_off = 0.45, show_prc = False) : 
-    
-    b = re.findall(r"[A-Za-z0-9]+", str(model))
-    if smote == True:
-        print("SMOTE")
-        print("--------")
-        from imblearn.over_sampling import SMOTE
-        smote = SMOTE(random_state=42 , k_neighbors=15)
-        X_train, y_train = smote.fit_resample(X_train, y_train)
-        
-    lr = model.fit(X_train, y_train)
-    print("train score %.2f" %lr.score(X_train, y_train))
-    print("--------")
-    
-        #PREDICTION DES Y_PRED
-    train_pred = lr.predict_proba(X_train)[:,1]
-    test_pred = lr.predict_proba(X_test)[:,1]
-    print("test score %.2f" %lr.score(X_test, y_test))
-    
-    print("--------")
-    print("Cut-off : ", cut_off)
-    
-        #ACCURACY, PRECISION ET TOUTE LA CLIQUE AU SEUIL DE 50% DE CONFIANCE (STANDARD)
-    print("--------")
-    print("classification_report\n")
-    test_pred_b = (test_pred > cut_off).astype(bool)
-    train_pred_b = (train_pred > cut_off).astype(bool)
-    print(classification_report(y_test, test_pred_b))
-    print("\n")
-
-    train_pred_b = (train_pred > cut_off).astype(bool)
-    
-            #MATRICE DE CONFUSION
-    if show_conf_matrix == True : 
-        print("Matrice de confusion\n")
-        print(confusion_matrix(y_test, test_pred_b))
-        print("--------")
-        #AUC
-    fpr, tpr, _= roc_curve(y_test, test_pred)
-    roc_auc = auc(fpr, tpr)
-    fpr_, tpr_, _train= roc_curve(y_train, train_pred)
-    roc_auc_ = auc(fpr_, tpr_)
-    print("L'AUC est de %.2f" %roc_auc)
-    print("--------")
-    Gini = roc_auc * 2 - 1
-    print('Gini: %.2f' % (Gini))
-    print("--------")
-    
-    if show_roc == True:
-        print('roc curve')
-        
-        plt.figure(figsize=(6,6))
-        plt.xlim([-0.01, 1.00])
-        plt.ylim([-0.01, 1.01])
-        plt.plot(fpr, tpr, lw=3, label='roc curve - Test (auc = {:0.2f})'.format(roc_auc))
-        #plt.plot(fpr_, tpr_, lw=3, label='roc curve - Train (auc = {:0.2f})'.format(roc_auc_))
-        plt.xlabel('False Positive Rate', fontsize=16)
-        plt.ylabel('True Positive Rate', fontsize=16)
-        plt.title(b[0], fontsize=16)
-        plt.legend(loc='lower right', fontsize=13)
-        plt.plot([0, 1], [0, 1], color='navy', lw=3, linestyle='--')
-        plt.axes().set_aspect('equal')
-        plt.show()
-        print("\n")
-        
-       
-    precision, recall, thresholds = precision_recall_curve(y_test, test_pred)
-    precision_, recall_, thresholds_ = precision_recall_curve(y_train, train_pred)
-    
-    closest_zero = np.argmin(np.abs(thresholds))
-    closest_zero_p = precision[closest_zero]
-    closest_zero_r = recall[closest_zero]
-    
-    closest_zero_ = np.argmin(np.abs(thresholds_))
-    closest_zero_p_ = precision_[closest_zero_]
-    closest_zero_r_ = recall_[closest_zero_]
-    
-    if show_prc == True : 
-        
-        print("precision_recall_curve")
-        plt.figure(figsize = (6,6))
-        plt.xlim([0.0, 1.01])
-        plt.ylim([0.0, 1.01])
-        plt.plot(precision, recall, label='prc - Test')
-        plt.xlabel('Precision', fontsize=16)
-        plt.ylabel('Recall', fontsize=16)
-        plt.axes().set_aspect('equal')
-        plt.legend(loc='lower left', fontsize=13)
-        plt.show()
-
-    J = tpr - fpr
-    ix = np.argmax(J)
-    seuil_opt = thresholds[ix]
-    
-    J_ = tpr_ - fpr_
-    ix_ = np.argmax(J_)
-    seuil_opt_ = thresholds_[ix_]
-    print('Meilleur cut-off test : %f' % (seuil_opt))
-    print('Meilleur cut-off train : %f' % (seuil_opt_))
-    
-    return fpr, tpr, roc_auc
-"""
